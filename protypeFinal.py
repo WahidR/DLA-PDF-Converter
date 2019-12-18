c3bdc940e23bf277f6bc7240adbe3ea82d2466ac
WahidR <57428330+WahidR@users.noreply.github.com>
2019-12-18 14:09:50 -0500

    Add files via upload

--- protypeFinal.py
+++ protypeFinal.py
@@ -6,14 +6,14 @@
 #     #itemArrayFormat = ["Item#", "Quantity", "8A Quantity", "SA Quantity", "Unrestricted", "Period of Performance",
 #     #                   "NSN", "Delivery Identification", "State", "Region", "Throughput SPLC", "Requirement SPLC",
 #     #                  "Delivery Address",
-#     #                   "Service Code", "Delivery DODAAC", "Ordering Office DODAAC", "Notes"]
+#     #                   "Service Code", "Delivery DODAAC", "Ordering Office DODAAC", "[mode1, mode2, notes]"]
 #
 #
 #     # modesFormat is another array because there can be multiple modes in each item. Each mode will be in a mode list
 #    # modesFormat = ["Mode", "Reciept %", "Max Parcel", "MinParcel", "FOB Restriction", "FSII", "SDA", "CI"]
 #
 #    # mode list will be [mode1, mode2, notes] or [mode1, notes]
-#     # Delivery Notes and Delivery Hours for each item will be one single string
+#     Delivery Notes and Delivery Hours for each item will be one single string and will be in the modeinfo list
 #     #notes = "Delivery Hours: 7AM - 12PM Delivery Notes: blablabla"
 # ____________________________________________________________________________________________________________________
 
@@ -25,6 +25,7 @@ def main():
     filename = 'WESTPAC_sol.pdf'
     clean_result = dataExtraction(filename)
     mainlist = []
+
     for i in range(len(clean_result)):
         item = clean_result[i]
         #print(f"Item {i}: ")
@@ -34,28 +35,32 @@ def main():
         #print("0 to nsn")
         #print(extractToNSN(item))
         itemattributes += ((extractToNSN(item)))
-        print()
+
         #print("nsn to region")
         #print(extractNSNtoRegion(item))
         itemattributes += (extractNSNtoRegion(item))
-        print()
+
         #print("Region to Service Code:")
         #print(extractRegionToService(item))
         itemattributes += (extractRegionToService(item))
-        print()
+
         #print("Service Code to Mode")
         #print(extractServiceCodeToMode(item))
         itemattributes += (extractServiceCodeToMode(item))
-        #print()
+
+        #print("Mode to End")
+        #print(extractModeToEnd(item))
+        #print("\n")
+        itemattributes += (extractModeToEnd(item))
+
         mainlist.append(itemattributes)
 
 
 
     print("THIS IS MAINLIST")
-    print(mainlist[0][6:10])
-    print(mainlist[1][6:10])
-    print(mainlist[2][6:10])
-    print(mainlist[12][6:10])
+    print(mainlist[0])
+    print(mainlist[1])
+
 
 
 def dataExtraction(filename):
@@ -172,6 +177,123 @@ def extractServiceCodeToMode(item):
         result[2] = ""
     return result
 
+def maxandminfinder(numbers):
+    result = ["max", "min"]
+    firstnum = ""
+    secondnum = ""
+    orginialnum = numbers
+    index = numbers.find(",")
+    foundfirstnum = False
+    if (numbers.find(",") < 0 and len(numbers) <= 3):
+        firstnum = numbers
+        secondnum = ""
+    elif (numbers.find(",") < 0):
+        firstnum = numbers[:3]
+        secondnum = numbers[3:]
+    else:
+
+        while (True):
+
+            commalocation = numbers.find(",")
+
+            if (numbers[index:].find(",") < 0):
+                firstnum = orginialnum
+                break
+            elif (numbers.find(",") > 3):
+                endindexoffirstnum = len(orginialnum) - len(numbers) + 3
+                firstnum = orginialnum[:endindexoffirstnum]
+                secondnum = orginialnum[endindexoffirstnum:]
+                break
+            else:
+                numbers = numbers[numbers.find(",") + 1:]
+    result[0] = firstnum
+    result[1] = secondnum
+
+    return result
+
+
+def extractModeToEnd(item):
+    focus = item[item.find("BULK:"):]
+
+
+    #   listofmodes = [mode1,mode2,delivery instructions]
+    #   len(listofmodes) will always be the number of modes + 1 b/c of delivery instructions
+    listofmodes = []
+
+    foclist = focus.split("BULK:")[1:]
+    # This loops through each mode
+    for i in range(len(foclist)):
+        currentmode = foclist[i]
+        splitmode = currentmode.split()
+        modeinfo = ["Mode", "Reciept%", "Max Parcel", "Min Parcel", "FOB Restriction", "FSII", "SDA", "CI"]
+
+        # This will repeat the extracting process according to the amount of modes there are
+        for j in range(len(splitmode)):
+            # Run everything under this
+            if len(foclist[i].split()) == 0:
+                continue
+            # This will always end up as
+            elif (len(foclist[i].split()) == 1):
+                modeinfo[0] = currentmode[:currentmode.find("100")]
+                # Reciept% is always 100
+                modeinfo[1] = "100"
+
+                if (currentmode[currentmode.find("100") + 3].isalpha()):
+                    modeinfo[2] = "0"
+                    modeinfo[3] = "0"
+                    modeinfo[4] = currentmode[currentmode.find("100") + 3]
+                    if (currentmode[len(currentmode) - 1] == 'Y'):
+                        modeinfo[5] = "Y"
+                    elif (currentmode[len(currentmode) - 1] == 'N'):
+                        modeinfo[5] = "N"
+                    else:
+                        modeinfo[5] = ""
+
+                else:
+                    reverse_modeinfo1 = splitmode[0][len(splitmode[0]) - 1:0]
+                    endindex = len(splitmode[0]) - (reverse_modeinfo1.find("0"))  # Shouldn't need a plus one
+                    numbers = currentmode[currentmode.find(100) + 3:endindex]
+                    min_max_array = maxandminfinder(numbers)
+                    modeinfo[2] = min_max_array[0]
+                    modeinfo[3] = min_max_array[1]
+                    modeinfo[4] = currentmode[endindex:endindex + 1]  # It is only the end index for the numbers slice
+                    modeinfo[5] = ""
+                modeinfo[6] = ""
+                modeinfo[7] = ""
+
+
+
+            else:
+                # modeinfo = ["Mode", "Reciept%", "Max Parcel", "Min Parcel", "FOB Restriction", "FSII", "SDA", "CI"]
+
+                modeinfo[0] = splitmode[0][:splitmode[0].find("100")]
+                # Reciept% is always 100
+                modeinfo[1] = "100"
+                remainingstring = splitmode[0][splitmode[0].find("100")]
+                reverse_modeinfo1 = splitmode[0][::-1]
+                endindex = len(splitmode[0]) - (reverse_modeinfo1.find("0"))  # Shouldn't need a plus one
+
+                numbers = currentmode[currentmode.find("100") + 3:endindex]
+                min_max_array = maxandminfinder(numbers)
+
+                modeinfo[2] = min_max_array[0]
+                modeinfo[3] = min_max_array[1]
+                modeinfo[4] = currentmode[endindex:endindex + 1]
+                modeinfo[5] = splitmode[0][len(splitmode[0]) - 1:]
+                modeinfo[6] = splitmode[1]
+                modeinfo[7] = splitmode[2]
+
+        listofmodes.append(modeinfo)
+
+    # Delivery Notes extraction:
+
+    if (focus.find("Delivery") >= 0):
+        delivery_notes = focus[focus.find("Delivery"):]
+    else:
+        delivery_notes = "No Delivery Notes"
+    listofmodes.append(delivery_notes)
+
+    return listofmodes
 
 
 if __name__ == '__main__':
